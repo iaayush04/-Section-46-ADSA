31ace12582d5991ceb59a6c07a8866d62b68b905
Ayush katheria <ayushkatheriya2022@gmail.com>
2025-09-29 17:29:14 +0530

    Create Substring with concatenation of all words.java

---
+++ Substring with concatenation of all words.java
@@ -0,0 +1,94 @@
+// You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+// A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+//     For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
+
+// Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: s = "barfoothefoobarman", words = ["foo","bar"]
+
+// Output: [0,9]
+
+// Explanation:
+
+// The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+// The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+
+// Example 2:
+
+// Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+
+// Output: []
+
+// Explanation:
+
+// There is no concatenated substring.
+
+// Example 3:
+
+// Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+
+// Output: [6,9,12]
+
+// Explanation:
+
+// The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"].
+// The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"].
+// The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"].
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() == 0 || words == null || words.length == 0) return result;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        if (s.length() < totalLen) return result;
+
+        Map<String, Integer> wordFreq = new HashMap<>();
+        for (String word : words) {
+            wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, count = 0;
+            Map<String, Integer> window = new HashMap<>();
+
+            for (int right = i; right + wordLen <= s.length(); right += wordLen) {
+                String word = s.substring(right, right + wordLen);
+
+                if (wordFreq.containsKey(word)) {
+                    window.put(word, window.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (window.get(word) > wordFreq.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        window.put(leftWord, window.get(leftWord) - 1);
+                        count--;
+                        left += wordLen;
+                    }
+
+                    if (count == wordCount) {
+                        result.add(left);
+                    }
+                } else {
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
