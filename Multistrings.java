7ae2af1b965dfa2bc0b5985aee64dbbc005d5d3a
Ayush katheria <ayushkatheriya2022@gmail.com>
2025-10-01 22:38:13 +0530

    Create Multistrings.java

---
+++ Multistrings.java
@@ -0,0 +1,43 @@
+// Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+// Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+ 
+
+// Example 1:
+
+// Input: num1 = "2", num2 = "3"
+// Output: "6"
+
+// Example 2:
+
+// Input: num1 = "123", num2 = "456"
+// Output: "56088"
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        
+        int m = num1.length(), n = num2.length();
+        int[] res = new int[m + n];
+       
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = mul + res[i + j + 1];
+                
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+       
+        StringBuilder sb = new StringBuilder();
+        for (int num : res) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+        
+        return sb.toString();
+    }
+}   
