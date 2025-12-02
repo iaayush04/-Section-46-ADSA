30d9f0f2902c7eb59c6ecb5c2b34582f017521e4
Ayush katheria <ayushkatheriya2022@gmail.com>
2025-12-02 11:32:04 +0530

    Add method to count segments in a string
    Implement a method to count segments in a string based on non-space characters.

---
+++ Number of Segments in a string.java
@@ -0,0 +1,29 @@
+// Given a string s, return the number of segments in the string.
+
+// A segment is defined to be a contiguous sequence of non-space characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello, my name is John"
+// Output: 5
+// Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+
+// Example 2:
+
+// Input: s = "Hello"
+// Output: 1
+
+class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
