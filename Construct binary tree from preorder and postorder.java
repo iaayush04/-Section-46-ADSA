f034d3bd2cefc5d7c662a5cc6c48f3f82981b74d
Ayush katheria <ayushkatheriya2022@gmail.com>
2026-02-22 23:12:08 +0530

    Add binary tree reconstruction from preorder and postorder
    Implement a solution to reconstruct a binary tree from preorder and postorder traversals.

---
+++ Construct binary tree from preorder and postorder.java
@@ -0,0 +1,39 @@
+// Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a binary tree of distinct values and postorder is the postorder traversal of the same tree, reconstruct and return the binary tree.
+
+// If there exist multiple answers, you can return any of them.
+
+ 
+
+// Example 1:
+
+// Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+// Output: [1,2,3,4,5,6,7]
+
+// Example 2:
+
+// Input: preorder = [1], postorder = [1]
+// Output: [1]
+class Solution {
+    
+    int preIndex = 0;
+    int postIndex = 0;
+    
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return build(preorder, postorder);
+    }
+    
+    private TreeNode build(int[] preorder, int[] postorder) {
+        TreeNode root = new TreeNode(preorder[preIndex++]);
+        
+        if (root.val != postorder[postIndex]) {
+            root.left = build(preorder, postorder);
+        }
+        
+        if (root.val != postorder[postIndex]) {
+            root.right = build(preorder, postorder);
+        }
+        
+        postIndex++;
+        return root;
+    }
+}
